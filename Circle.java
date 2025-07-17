41e61bd2e26ecbcaea5d8f3caf9d6984e4793b03
Aiden Phillips <aphil122@charlotte.edu>
2025-07-17 11:13:52 -0400

    ProblemSolve19

--- Circle.java
+++ Circle.java
@@ -1,47 +1,76 @@
 import java.awt.Color;
 
 /**
- * This class describes a circle with a given radius and color.
- *
+ * Represents a circle with a specified radius and color.
+ * Provides methods to access the radius, color, and area of the circle.
+ * 
+ * This class calculates the area internally and exposes it through a getter.
+ * 
  * @author nanajjar
  * @version Spring 25
  */
-
-public class Circle {  
-    // private instance variable, not accessible from outside this class
+public class Circle {
+    /** The radius of the circle */
     private double radius;
+
+    /** The color of the circle */
     private Color color;
+
+    /** The computed area of the circle */
     private double area;
 
-    // The default constructor with no argument.
-    // It sets the radius and color to their default value.
+    /**
+     * Constructs a default Circle object with a radius of 1.0 and
+     * a default color (orange).
+     */
     public Circle() {
         radius = 1.0;
-        this.color = new Color(255,119,65);
+        this.color = new Color(255, 119, 65);
     }
 
-    // 2nd constructor with given radius and color
+    /**
+     * Constructs a Circle with a specified radius and color.
+     *
+     * @param r    the radius of the circle
+     * @param clr  the color of the circle
+     */
     public Circle(double r, Color clr) {
         radius = r;
         color = clr;
     }
 
-    // A public method for retrieving the radius
+    /**
+     * Returns the radius of the circle.
+     *
+     * @return the radius
+     */
     public double getRadius() {
         return radius;
     }
 
-    // A public method for retrieving the color of circle
+    /**
+     * Returns the color of the circle.
+     *
+     * @return the color
+     */
     public Color getColor() {
         return color;
     }
 
-    // A public method for retrieving the area of circle
+    /**
+     * Returns the area of the circle.
+     * Triggers calculation before returning the result.
+     *
+     * @return the area of the circle
+     */
     public double getArea() {
         calculateArea();
         return area;
     }
-    // A private method for computing the area of circle
+
+    /**
+     * Calculates the area of the circle using the formula: π × radius²
+     */
     private void calculateArea() {
         area = radius * radius * Math.PI;
     }
---
+++ README.md
@@ -0,0 +1,5 @@
+Aiden Phillips, aphil122@charlotte.edu, 801289463, AphilCLT, Problem Solve 19.
+
+The RectangleText.java program creates a object scanner called input that prompts the user for two inputs width and height.
+
+The Circle.java program is a reusable class that represents a circle. It allows to create circle iwth different size, color, area.
